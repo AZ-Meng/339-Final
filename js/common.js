668f5197f7cdbba92a4786505ac8daebf940f126
Meng Zhao <jpja19981224@gmail.com>
2021-04-16 09:29:33 +0800

    html validation

--- js/common.js
+++ js/common.js
@@ -7,3 +7,14 @@ function showMenu(){
  	}
  	show = !show;
 }
+
+
+show2 = false;
+function showMenu2(){
+ 	if (show2 == false){
+  		document.querySelector("#dropDown_navi").style.display="block";
+ 	} else{
+  		document.querySelector("#dropDown_navi").style.display="none";
+ 	}
+ 	show2 = !show2;
+}
